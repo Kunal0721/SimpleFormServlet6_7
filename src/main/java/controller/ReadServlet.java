86fa9ecbcfd9134a178804f5849a4d8376adc7fa
kunal0721 <kunalbamoriya984@gmail.com>
2026-05-28 18:35:17 +0530

    config work is added

--- src/main/java/controller/ReadServlet.java
+++ src/main/java/controller/ReadServlet.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import entity.Student;
 import jakarta.servlet.ServletException;
@@ -11,35 +12,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.StudentDAO;
 
-@WebServlet("/abc")
-public class SimpleServlet extends HttpServlet {
+@WebServlet("/read")
+public class ReadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private StudentDAO dao = new StudentDAO();
 
-	public SimpleServlet() {
+	public ReadServlet() {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		String name = request.getParameter("nm");
-		int age = Integer.parseInt(request.getParameter("age"));
-		String course = request.getParameter("course");
-
-		StudentDAO dao = new StudentDAO();
-		Student st = new Student(name, age, course);
-		int i = dao.insert(st);
 		PrintWriter out = response.getWriter();
-		if (i != 0) {
-			request.getRequestDispatcher("read").forward(request, response);
-			
-		} else {
-			out.println("<h1> Something went wrong </h1>");
+
+		out.println("<h1 align='center'> Success </h1>");
+
+		List<Student> ls = dao.getAll();
+		out.println("<center>");
+		for (Student s : ls) {
+
+			out.println(s.getId() + "   " + s.getName() + "   " + s.getAge() + "   " + s.getCourse() + "<br>");
+
 		}
+		out.println("</center>");
 	}
 
 }
